da95ff3d5beb88cbe00607e1f94d439d6db317c1
Erik Kalkoken <erik.kalkoken@gmail.com>
2019-08-30 01:10:55 +0200

    replaced logo, improved upload script

--- upload.bat
+++ upload.bat
@@ -2,4 +2,4 @@
 rem puts _site into a ZIP file and uploads to webserver for deployment
 del _site.zip
 "C:\Program Files\WinZip\WINZIP64.EXE" -min -a -r _site.zip _site\*.*
-"C:\Program Files\CoreFTP\coreftp.exe" -s -O -site euve266768_(erik997) -u D:\Users\bji74\Documents\Jekyll\www-0-cal-net\_site.zip -p /home/erik997/www-0-cal-net -log
+"C:\Program Files\CoreFTP\coreftp.exe" -s -O -site euve266768_0-cal -u D:\Users\bji74\Documents\Jekyll\www-0-cal-net\_site.zip -p /home/erik997/www-0-cal-net -log
